030d9762754354a6b1af6c180690532e09c5c1a9
Rohit Singh <hitop2509@gmail.com>
2023-07-28 01:06:51 +0530

    navbar for pc added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,25 @@
 import { NavLink } from "react-router-dom";
-
+import "./Navbar.css";
 const Navbar = () => {
     return (
-        <nav>
-            <div className="logo">
-                <span className="fName">Rohit</span>
-                <span className="lName">Singh</span>
+        <nav className="navbar">
+            <div className="navContainer">
+                <div className="logo">
+                    <span className="fName">ROHIT</span>
+                    <span className="lName">SINGH</span>
+                </div>
+                <ul className="navLinks">
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/projects">Projects</NavLink>
+                    </li>
+                </ul>
             </div>
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/projects">Projects</NavLink>
-                </li>
-            </ul>
         </nav>
     );
 };
